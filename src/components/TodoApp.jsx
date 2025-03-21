47eb3c169be3a3bc30fa755cad7e4cd600b1d28c
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-21 23:41:40 +0100

    TodoApp

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,18 +1,65 @@
+import { useState } from "react";
+
 const TodoApp = () => {
+  /* const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Apprendre React JS'
+    },
+    {
+        id: 2,
+        text: 'Faire des tests unitaires'
+    },
+    {
+        id: 3,
+        text: 'Courir 10 km'
+    }
+  ]);*/
+  const [tasks, setTasks] = useState([]);
+//   console.log('tasks', tasks);
+  const [taskText, setTaskText] = useState('');
+
+  const addTask = (e) => {
+    e.preventDefault();
+    // setTasks([])
+    tasks.push({ id: Date.now(), text: taskText });
+    console.log(tasks);
+  }
+
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setTaskText(e.target.value);
+  }
   return (
     <div className="max-w-md mx-auto mt-10 bg-white shadow-lg p-6 rounded-lg">
         <h1 className="text-center text-2xl font-bold text-green-500">Ma Todo List</h1>
 
-        <form className="flex gap-2 m-6 px-4 py-2">
+        <form onSubmit={addTask} className="flex gap-2 m-6 px-4 py-2">
             <input type="text" 
                 className="border border-gray-300 rounded-md flex-grow focus:outline-none" 
                 placeholder="Ajouter une tâche ..."
+                value={taskText}
+                onChange={handleChange}
             />
             <button type="submit" 
                 className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700">
                     Ajouter
             </button>
         </form>
+
+        {/* Liste des tâches */}
+        <ul>
+            {tasks.map((task) => (
+                <li key={task.id} 
+                    className="flex justify-between bg-gray-500 px-3 py-1 rounded-lg mb-1 text-white">
+                    {task.text}
+                </li>
+            ))}
+        </ul>
+
+        {tasks.length === 0 && (
+            <p className="text-center text-gray-500">Aucune tâche pour le moment.</p>
+        )}
     </div>
   )
 }
