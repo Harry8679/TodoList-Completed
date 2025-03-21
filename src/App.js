c1a520e2541c0fcf4768f6e751a95e4d3119ad90
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-21 12:13:56 +0100

    Update 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <TodoList />
   );
 }
 
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import TaskList from "./TaskList";
+
+const TodoList = () => {
+  return (
+    <div className="max-w-2xl mx-auto mt-10 p-6 bg-white shadow-md rounded-lg">
+        <h1 className="text-2xl font-bold text-center mb-4">Todo List</h1>
+        <div className="flex gap-2 mb-4">
+            <input type="text" 
+                placeholder="Ajouter une tâche ..." 
+                className="border rounded-lg px-3 py-2 flex-grow"/>
+            <button 
+                className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-lg">
+                    Ajouter
+            </button>
+        </div>
+        <TaskList />
+    </div>
+  )
+}
+export default TodoList;
