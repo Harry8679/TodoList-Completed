fad1c2a4c87ee162e2d75b35bbc63d7dd3e5da48
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-24 17:26:37 +0100

    Update 2

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,6 +1,24 @@
-const TaskItem = () => {
+const TaskItem = ({ task,editingId, editText, setEditText, startEditing, deleteTask, saveEdit }) => {
   return (
-    <div>TaskItem</div>
+    <li className="flex justify-between items-center bg-gray-200 rounded-md my-2 p-2">
+        {editingId === task.id ? (
+            <input type="text" 
+                className="border px-2 py-1 rounded-lg flex-grow" 
+                value={editText}
+                onChange={(e) => setEditText(e.target.value)}
+            />
+        ): (
+            <span>{task.text}</span>
+        )}
+        <div className="flex gap-2">
+            {editingId === task.id ? (
+                <button className="bg-green-500 text-white px-3 py-1 rounded-lg">Sauvegarder</button>
+            ):(
+                <button className="bg-yellow-500 text-white px-3 py-1 rounded-lg">Modifier</button>
+            )}
+            <button className="bg-red-500 text-white px-3 py-1 rounded-lg">Supprimer</button>
+        </div>
+    </li>
   )
 }
 export default TaskItem;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,19 @@
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, editingId, editText, setEditText, startEditing, deleteTask, saveEdit }) => {
   return (
     <ul>
         {tasks.map((task) => (
-            <TaskItem />
+            <TaskItem 
+                key={task.id}
+                task={task}
+                editingId={editingId}
+                editText={editText}
+                setEditText={setEditText}
+                startEditing={startEditing}
+                deleteTask={deleteTask}
+                saveEdit={saveEdit}
+            />
         ))}
     </ul>
   )
